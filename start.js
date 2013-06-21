bf69e1ecd8c7e9904e1e64772cea14cc41ebd773
George Czabania <george@czabania.com>
2013-06-21 13:38:18 +1200

    Make start.js a useful tool

--- start.js
+++ start.js
@@ -1,15 +1,21 @@
 
 var Core = require('./bin/core');
+var Queue = require('./bin/queue');
 var Methods = require('./bin/methods');
 var SongList = require('./bin/models/songList');
 
-var http = require('http');
 var fs = require('fs');
+var when = require('when');
 
 var core = new Core();
 var app = new Methods(core);
 
 var _ = {};
+
+var config = {
+  id: 87558723,
+  type: 'playlist'
+};
 
 function log() {
   var args = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
@@ -17,80 +23,70 @@ function log() {
   console.log.apply(console, args);
 }
 
+// There seems to be a problem with downloads getting corrupted.
+// Not sure why that happens. Will need to look into it
+var queue = new Queue(1, function(song) {
+  var deferred = when.defer();
+
+  songId = song.SongID;
+  songName = song.Name;
+  songArtist = song.ArtistName;
+  songAlbum = song.AlbumName;
+
+  log('\n');
+  log('ID:', songId);
+  log('Name:', songName);
+  log('Artist:', songArtist);
+  log('Album:', songAlbum);
+
+  if (songId === undefined) {
+    console.log(song);
+  }
+
+  app.getSongStream(songId).then(function(stream) {
+    console.log('Got stream');
+    var path = 'dl/' + songArtist + ' - ' + songName + '.mp3';
+    var file = fs.createWriteStream(path);
+    stream.on('data', function(chunk) {
+      file.write(chunk);
+    });
+    stream.on('end', function() {
+      file.end();
+      deferred.resolve();
+    });
+  }).otherwise(function(err) {
+    throw err;
+  });
+
+  return deferred.promise;
+});
+
 core.init().then(
   function() {
-    console.log('Ready');
-    return app.getPlaylistSongs(40354457);
+    console.log('> Groovy is ready!');
+    switch ( config.type ){
+      case 'playlist':
+        return app.getPlaylistSongs( config.id );
+      case 'album':
+        return app.getAlbumSongs( config.id );
+    }
   }
 ).then(
   function(response) {
-    var songs = new SongList(response.Songs);
-    _.song = songs[348];
-    _.songId = _.song.SongID;
-    _.songName = _.song.Name;
-    _.songArtist = _.song.ArtistName;
-    _.songAlbum = _.song.AlbumName;
-    log('ID:', _.songId);
-    log('Name:', _.songName);
-    log('Artist:', _.songArtist);
-    log('Album:', _.songAlbum);
+    switch ( config.type ) {
+      case 'playlist':
+        var songs = response.Songs;
+        break;
+      case 'album':
+        var songs = [];
+        for (var id in response.songs) {
+          if (response.songs.hasOwnProperty(id) && id !== 'length') {
+            songs.push(response.songs[id]);
+          }
+        }
+        break;
+    }
+    queue.add(songs);
+    queue.start();
   }
-)
-// .then(
-//   function(fileData) {
-//     log('\n\nDownload has finished');
-//     var fileName = './cache/' + _.songArtist + ' - ' + _.songName +'.mp3';
-//     console.log('Saving ' + fileName);
-//     fs.writeFile(fileName, fileData, { encoding: 'binary' });
-//   },
-//   null,
-//   function(pos) {
-//     var graph = '';
-//     var i = 0;
-//     while (i < pos) {
-//       graph += '=';
-//       i++
-//     }
-//     graph += new Array(100-pos).join(' ');
-//     process.stdout.write("[" + graph + "] " + pos + "%\r");
-//   }
-// )
-
-
-http.createServer(function(req, res) {
-
-  console.log('Got http request');
-
-  if (req.url.slice(-4) === '.mp3') {
-
-    app.getSongStream(_.songId).then(function(stream) {
-
-      console.log('Got stream request');
-
-      var length = stream.headers['content-length'];
-      console.log(length);
-
-      res.writeHead(200, {
-        "Content-Type": "audio/mpeg",
-        "Content-Length": length
-      });
-
-      stream.on('data', function(chunk) {
-        res.write(chunk);
-      });
-
-      stream.on('end', function() {
-        console.log('Finished!');
-        res.end();
-      })
-
-    });
-
-  } else {
-
-    res.write('404');
-    res.end();
-
-  }
-
-}).listen(8080);
+);
